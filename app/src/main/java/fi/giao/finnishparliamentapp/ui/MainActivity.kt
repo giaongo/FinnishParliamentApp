8240135a240b1e919700f2784edd9dbf88862891
Giao Ngo <giao.ngo@metropolia.fi>
2022-09-24 14:05:05 +0300

    Add list of important fragments to the app

--- app/src/main/java/fi/giao/finnishparliamentapp/ui/MainActivity.kt
+++ app/src/main/java/fi/giao/finnishparliamentapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package fi.giao.finnishparliamentapp
+package fi.giao.finnishparliamentapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import fi.giao.finnishparliamentapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/fi/giao/finnishparliamentapp/ui/PartyListFragment.kt
@@ -0,0 +1,23 @@
+package fi.giao.finnishparliamentapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import fi.giao.finnishparliamentapp.R
+
+class PartyListFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_party_list, container, false)
+    }
+
+}
