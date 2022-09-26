eaf71bcb494a5c000f5a1e4adb5305fc4a07005b
Giao Ngo <giao.ngo@metropolia.fi>
2022-09-26 23:19:02 +0300

    Initialize ViewModel class in the fragment. Fix incompatibility error in Room and Coroutine by upgrading the Room version to 2.4.3 and add coroutine dependencies . Add Internet permission for network data fetch. Contribute to #4 and #9

--- app/src/main/java/fi/giao/finnishparliamentapp/ui/fragments/PartyListFragment.kt
+++ app/src/main/java/fi/giao/finnishparliamentapp/ui/fragments/PartyListFragment.kt
@@ -1,25 +1,46 @@
 package fi.giao.finnishparliamentapp.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import fi.giao.finnishparliamentapp.adapter.PartyAdapter
 import fi.giao.finnishparliamentapp.databinding.FragmentPartyListBinding
+import fi.giao.finnishparliamentapp.viewmodel.MemberViewModel
+import fi.giao.finnishparliamentapp.viewmodel.MemberViewModelFactory
 
 class PartyListFragment : Fragment() {
     private lateinit var binding: FragmentPartyListBinding
-
+    private val viewModel: MemberViewModel by activityViewModels {
+        MemberViewModelFactory(requireActivity().application)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentPartyListBinding.inflate(inflater,container,false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val memberAdapter = PartyAdapter()
+        binding.partyListRecyclerView.apply {
+            adapter = memberAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+        viewModel.memberList.observe(viewLifecycleOwner) { list ->
+            run {
+                Toast.makeText(requireContext(),"Total: ${list.size}",Toast.LENGTH_SHORT).show()
+                val partyList = list.map { it.party }.toSet().toList().sorted()
+                memberAdapter.submitList(partyList)
+            }
+        }
+
     }
 }
--- app/src/main/java/fi/giao/finnishparliamentapp/viewmodel/MemberViewModel.kt
+++ app/src/main/java/fi/giao/finnishparliamentapp/viewmodel/MemberViewModel.kt
@@ -4,15 +4,36 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import fi.giao.finnishparliamentapp.database.AppDatabase
 import fi.giao.finnishparliamentapp.database.ParliamentMember
 import fi.giao.finnishparliamentapp.repository.AppRepository
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 class MemberViewModel(application: Application): AndroidViewModel(application) {
 
     private val appRepository =  AppRepository(AppDatabase.getInstance(application))
 
-    private val _memberList = MutableLiveData<List<ParliamentMember>>()
-    val memberList: LiveData<List<ParliamentMember>> = _memberList
+    val memberList:LiveData<List<ParliamentMember>> =  appRepository.getAllMembers()
+
+    private val _status = MutableLiveData<String>()
+    val status:LiveData<String> = _status
+
+    init {
+        getDataFromNetworkAndSave()
+    }
+
+    private fun getDataFromNetworkAndSave() {
+        viewModelScope.launch {
+            try {
+                appRepository.getDataFromNetworkAndSave()
+                _status.value = "fetching data successfully"
+            } catch (error:IOException) {
+                _status.value = error.toString()
+            }
+        }
+    }
+
 }
 
