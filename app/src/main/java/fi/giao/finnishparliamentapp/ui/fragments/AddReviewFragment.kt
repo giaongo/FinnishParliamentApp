81779f9bc9e351b8c655dbbc67a914d4a29954d5
Giao Ngo <giao.ngo@metropolia.fi>
2022-09-26 10:29:01 +0300

    Add 2 more fragments for app and make its first layout look

---
+++ app/src/main/java/fi/giao/finnishparliamentapp/ui/fragments/AddReviewFragment.kt
@@ -0,0 +1,24 @@
+package fi.giao.finnishparliamentapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import fi.giao.finnishparliamentapp.R
+
+class AddReviewFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_review, container, false)
+    }
+
+}
