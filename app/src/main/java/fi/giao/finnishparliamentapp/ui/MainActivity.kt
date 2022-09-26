c27139f19b9b84c13fb3dfab57b6bd8a8a837491
Giao Ngo <giao.ngo@metropolia.fi>
2022-09-26 08:53:03 +0300

    some changes in MainActivity class

---
+++ app/src/main/java/fi/giao/finnishparliamentapp/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package fi.giao.finnishparliamentapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import fi.giao.finnishparliamentapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
