45772ddb6943dd806aedaefe5f026501f82d5f8e
Giao Ngo <giao.ngo@metropolia.fi>
2022-09-29 22:31:32 +0300

    Add button click in AddReviewFragment.kt and functions in repository. Contribute to #11

--- app/src/main/java/fi/giao/finnishparliamentapp/repository/AppRepository.kt
+++ app/src/main/java/fi/giao/finnishparliamentapp/repository/AppRepository.kt
@@ -1,22 +1,27 @@
 package fi.giao.finnishparliamentapp.repository
 
 import fi.giao.finnishparliamentapp.database.AppDatabase
-import fi.giao.finnishparliamentapp.database.ParliamentMember
+import fi.giao.finnishparliamentapp.database.MemberReview
 import fi.giao.finnishparliamentapp.network.MemberApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class AppRepository(private val database: AppDatabase) {
-    fun getAllMembers() = database.memberDAO.getAllMembers()
+    fun getAllMembers() = database.memberDao.getAllMembers()
 
     /* Function that retrieves data from network and save to local database.
     * This function uses IO dispatcher to make sure it is safe to call from main thread*/
     suspend fun getDataFromNetworkAndSave() {
         withContext(Dispatchers.IO) {
             val memberList = MemberApi.retrofitService.getMembers()
-            database.memberDAO.insertAllMembers(memberList)
+            database.memberDao.insertAllMembers(memberList)
         }
     }
 
-    fun getMembersFromParty(party:String) = database.memberDAO.getMembersFromParty(party)
+    fun getMembersFromParty(party:String) = database.memberDao.getMembersFromParty(party)
+    fun getAllReviewsByHetekaId(hetekaId:Int) = database.reviewDao.getAllReviewsByHetekaId(hetekaId)
+    suspend fun insertReview(review:MemberReview) = database.reviewDao.insertReview(review)
+    suspend fun updateReview(review:MemberReview) = database.reviewDao.updateReview(review)
+    suspend fun deleteReview(review:MemberReview) = database.reviewDao.deleteReview(review)
+
 }
--- app/src/main/java/fi/giao/finnishparliamentapp/ui/fragments/AddReviewFragment.kt
+++ app/src/main/java/fi/giao/finnishparliamentapp/ui/fragments/AddReviewFragment.kt
@@ -1,24 +1,37 @@
 package fi.giao.finnishparliamentapp.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import fi.giao.finnishparliamentapp.R
+import android.widget.Toast
+import fi.giao.finnishparliamentapp.databinding.FragmentAddReviewBinding
 
 class AddReviewFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
+    private lateinit var binding: FragmentAddReviewBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_review, container, false)
+        binding = FragmentAddReviewBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /**
+         * Set listener to be called when the rating changes
+         * Reference
+         * https://developer.android.com/reference/android/widget/RatingBar#setOnRatingBarChangeListener(android.widget.RatingBar.OnRatingBarChangeListener)
+         *
+         */
+        binding.starRatingBar.setOnRatingBarChangeListener { _, rating, _ ->
+            Toast.makeText(requireContext(), "You rate $rating stars",Toast.LENGTH_SHORT).show()
+        }
+
+    }
 }
