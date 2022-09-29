54ba1267b62152fae52710456e6dafd6831dd58e
Giao Ngo <giao.ngo@metropolia.fi>
2022-09-29 22:30:18 +0300

    Initialize ViewModel and add recycler view adapter.Contribute to #11

---
+++ app/src/main/java/fi/giao/finnishparliamentapp/adapter/ReviewAdapter.kt
@@ -0,0 +1,48 @@
+package fi.giao.finnishparliamentapp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import fi.giao.finnishparliamentapp.R
+import fi.giao.finnishparliamentapp.database.MemberReview
+import fi.giao.finnishparliamentapp.databinding.ReviewBinding
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+
+class ReviewAdapter(val context: Context): ListAdapter<MemberReview, ReviewAdapter.ReviewViewHolder>(ReviewDiffCallBack) {
+    inner class ReviewViewHolder(private val binding: ReviewBinding):RecyclerView.ViewHolder(binding.root) {
+        private val simpleDateFormat = SimpleDateFormat("HH:mm" + "-dd.MM.yyyy")
+
+        fun bindData(review:MemberReview) {
+            val timestamp = simpleDateFormat.format(Timestamp(review.timeStamp))
+            binding.apply {
+                reviewRatingBar.rating = review.rating
+                reviewTextView.text = context.getString(R.string.review_text,review.comment)
+                reviewTimestamp.text = context.getString(R.string.review_timestamp,timestamp)
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
+        val view = ReviewBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ReviewViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
+        holder.bindData(getItem(position))
+    }
+
+    companion object ReviewDiffCallBack: DiffUtil.ItemCallback<MemberReview>() {
+        override fun areItemsTheSame(oldItem: MemberReview, newItem: MemberReview): Boolean {
+            return oldItem.reviewId == newItem.reviewId
+        }
+
+        override fun areContentsTheSame(oldItem: MemberReview, newItem: MemberReview): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
--- app/src/main/java/fi/giao/finnishparliamentapp/ui/fragments/MemberInfoFragment.kt
+++ app/src/main/java/fi/giao/finnishparliamentapp/ui/fragments/MemberInfoFragment.kt
@@ -5,14 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import fi.giao.finnishparliamentapp.R
 import fi.giao.finnishparliamentapp.database.ParliamentMember
 import fi.giao.finnishparliamentapp.databinding.FragmentMemberInfoBinding
+import fi.giao.finnishparliamentapp.viewmodel.MemberInfoViewModel
+import fi.giao.finnishparliamentapp.viewmodel.MemberInfoViewModelFactory
+
 private const val IMG_BASE_URL = "https://avoindata.eduskunta.fi/"
 class MemberInfoFragment : Fragment() {
     private lateinit var binding: FragmentMemberInfoBinding
+    private val viewModel:MemberInfoViewModel by viewModels {
+        MemberInfoViewModelFactory(requireActivity().application)
+    }
     private val safeArgs: MemberInfoFragmentArgs by navArgs()
     private lateinit var currentMember: ParliamentMember
 
@@ -25,7 +33,6 @@ class MemberInfoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         binding = FragmentMemberInfoBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -39,7 +46,13 @@ class MemberInfoFragment : Fragment() {
             )
             memberPartyTextView.text = getString(R.string.party_name_text, currentMember.party)
         }
+        // Fetch image data from network and bind it into imageView
         Glide.with(this).load(IMG_BASE_URL + currentMember.pictureUrl).into(binding.memberImageView)
+
+        binding.reviewAddButton.setOnClickListener {
+            val action = MemberInfoFragmentDirections.actionMemberInfoFragmentToAddReviewFragment()
+            view.findNavController().navigate(action)
+        }
 
 
     }
